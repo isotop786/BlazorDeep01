3b3b4fbd7b55f244ad74567708a1269d8866cdc5
isotop786 <marufislam77@gmail.com>
2025-08-12 00:02:58 +0200

    Add project files.

---
+++ BlazorDeep01/Models/Server.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorDeep01.Models
+{
+    public class Server
+    {
+
+        public Server()
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 2);
+
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+
+        public int ServerId { get; set; }
+
+        public bool IsOnline { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? City { get; set; }
+
+
+    }
+}
---
+++ BlazorDeep01/Models/SeverRepository.cs
@@ -0,0 +1,76 @@
+﻿namespace BlazorDeep01.Models
+{
+    public class SeverRepository
+    {
+
+        private static List<Server> servers = new List<Server>()
+        {
+            new Server {ServerId =1, Name ="Server1", City="Bruxelles"},
+            new Server {ServerId =1, Name ="Server2", City="Bruxelles"},
+            new Server {ServerId =1, Name ="Server3", City="Bruxelles"},
+            new Server {ServerId =1, Name ="Server4", City="Dhaka"},
+            new Server {ServerId =1, Name ="Server5", City="Bruxelles"},
+            new Server {ServerId =1, Name ="Server6", City="Dhaka"},
+            new Server {ServerId =1, Name ="Server7", City="Bruxelles"},
+        };
+
+        public static void AddServer(Server server)
+        {
+            var maxId = servers.Max(s => s.ServerId);
+            server.ServerId = maxId + 1;
+            servers.Add(server);
+        }
+
+        public static List<Server> GetServers() => servers;
+
+        public static List<Server> GetServerByCity(string cityName)
+        {
+            return servers.Where(s => s.City.Equals(cityName, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public static Server? GetServerById(int id)
+        {
+            var server = servers.FirstOrDefault(s => s.ServerId == id);
+            if(server != null)
+            {
+                return new Server
+                {
+                    ServerId = server.ServerId,
+                    Name = server.Name,
+                    City = server.City,
+                    IsOnline = server.IsOnline
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateServer(int serverId, Server server)
+        {
+            if (serverId != server.ServerId) return;
+
+            var serverToUpdate = servers.FirstOrDefault(s => s.ServerId == serverId);
+
+            if(serverToUpdate != null)
+            {
+                serverToUpdate.IsOnline = server.IsOnline;
+                serverToUpdate.Name = server.Name;
+                serverToUpdate.City = server.City;
+            }
+        }
+
+        public static void DeleteServer(int serverId)
+        {
+            var server = servers.FirstOrDefault(s => s.ServerId == serverId);
+            if(server != null)
+            {
+                servers.Remove(server);
+            }
+        }
+
+        public static List<Server> SearchServers(string serverFilter)
+        {
+            return servers.Where(s => s.Name.Contains(serverFilter, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+}
